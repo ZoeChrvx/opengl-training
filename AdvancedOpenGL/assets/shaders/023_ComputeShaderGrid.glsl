5aea1276dfe1187b71887f86231f5de40ecd8467
ZoeChrvx <zchervaux@artfx.fr>
2024-11-04 14:47:09 +0100

    [add] GridColors

--- AdvancedOpenGL/assets/shaders/023_ComputeShaderGrid.glsl
+++ AdvancedOpenGL/assets/shaders/023_ComputeShaderGrid.glsl
@@ -1,25 +1,37 @@
 #version 450
 
 // We will draw patches of 16*16 pixels, 80*45 times for a final 1280*720 image
-layout (local_size_x = 16, local_size_y = 16) in;
+layout(local_size_x = 16, local_size_y = 16) in;
 
 // No data input: the compute shader will create the image output.
-layout (rgba32f, binding = 0) uniform image2D img_output;
+layout(rgba32f, binding = 0) uniform image2D img_output;
 
 void main() {
-  // gl_LocalInvocationID.xy * gl_WorkGroupID.xy == gl_GlobalInvocationID
-  ivec2 coords = ivec2(gl_GlobalInvocationID);
+    // gl_LocalInvocationID.xy * gl_WorkGroupID.xy == gl_GlobalInvocationID
+    ivec2 coords = ivec2(gl_GlobalInvocationID);
 
-  // We want to draw an orange and blue grid
-  vec4 pixel;
-  // Orange alternance: when workgroup x is even we want workgroup y to be odd, and vice-versa
-  if ( ((gl_WorkGroupID.x & 1u) != 1u) != ((gl_WorkGroupID.y & 1u) == 1u)) {
-    pixel = vec4(1.0,.5,.0,1.0);
-  }
-  // Blue alternance in case orange condition is not met
-  else {
-    pixel = vec4(.0,.5,1.0,1.0);
-  }
+    // We want to draw an orange and blue grid
+    vec4 pixel;
 
-  imageStore(img_output, coords, pixel);
+    // Red alternance: when workgroup x is even we want workgroup y to be odd, and vice-versa
+    if (mod(gl_WorkGroupID.x + gl_WorkGroupID.y, 4) == 0)
+    {
+        pixel = vec4(0.22, 0.47, 0.18, 1.0);
+    }
+    // Green alternance in case orange condition is not met
+    else if (mod(gl_WorkGroupID.x + gl_WorkGroupID.y, 4) == 1)
+    {
+        pixel = vec4(0.98, 0.89, 0.52, 1.0);
+    }
+    // Blue
+    else if (mod(gl_WorkGroupID.x + gl_WorkGroupID.y, 4) == 2)
+    {
+        pixel = vec4(0.39, 0.92, 0.57, 1.0);
+    }
+    // White
+    else if (mod(gl_WorkGroupID.x + gl_WorkGroupID.y, 4) == 3)
+    {
+        pixel = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+    imageStore(img_output, coords, pixel);
 }
